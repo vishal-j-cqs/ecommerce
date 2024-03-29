cc244ad3b09abbceb9d986e8952ad6640863dc47
vishal161201 <ravijan1800@gmail.com>
2024-03-29 09:03:37 +0530

    changes

---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Populer from '../Components/Populer/Populer'
+
+const Shop = () => {
+  return (
+    <div>
+   <Hero/>
+   <Populer/>
+   </div>
+  )
+}
+
+export default Shop
