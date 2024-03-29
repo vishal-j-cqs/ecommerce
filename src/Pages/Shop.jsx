fd64ca592d116f5addd32aab87b9389b8d2b72e7
vishal161201 <ravijan1800@gmail.com>
2024-03-29 11:33:29 +0530

    changes

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Populer from '../Components/Populer/Populer'
+import Offers from '../Components/Offers/Offers'
 
 const Shop = () => {
   return (
     <div>
    <Hero/>
    <Populer/>
+   <Offers/>
    </div>
   )
 }
